ab1decb9eb45f8d7ac474bda16af279716cb7f3e
Adarsh Raj <132927594+AdarshRaj08@users.noreply.github.com>
2026-05-29 07:35:57 +0530

    Time: 3 ms (62.20%) | Memory: 8.3 MB (11.59%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     vector<int> dp; 
     int fib(int n) {
-        // create dp only once 
-        if(dp.empty())
-            dp.resize(n+1,-1);
-        if(n == 0 || n == 1)
-            return n;
+        // // create dp only once 
+        // if(dp.empty())
+        //     dp.resize(n+1,-1);
+        // if(n == 0 || n == 1)
+        //     return n;
         
-        if(dp[n] != -1)
-            return dp[n];
-        
-        dp[n] = fib(n-1) + fib(n-2);
+        // if(dp[n] != -1)
+        //     return dp[n];
+        // return dp[n] = fib(n-1) + fib(n-2);
 
-        return dp[n];
+        if(n==0)
+            return 0;
+        vector<int> dpp(n+1);
+        dpp[0] = 0; dpp[1] = 1;
+        for(int i=2; i<=n; i++)
+        {
+            dpp[i] = dpp[i-1] + dpp[i-2];
+        }
+        return dpp[n];
     }
 };
