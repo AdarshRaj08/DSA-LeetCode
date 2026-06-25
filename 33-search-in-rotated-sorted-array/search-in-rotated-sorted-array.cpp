26c7c1ddc81c2c739ed7f2fe3e62669400737744
Adarsh Raj <132927594+AdarshRaj08@users.noreply.github.com>
2026-06-25 14:32:11 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (67.18%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,30 +1,50 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        int l = 0,r = n-1;
+    int pivot(vector<int>& nums,int n)
+    {
+        int l=0,r=n-1;
 
+
+        while(l < r)
+        {
+            int mid = l + (r-l)/2;
+
+            if(nums[mid] < nums[r])
+                r = mid;
+            else
+                l = mid+1;
+        }
+        return r;
+    }
+    int bsearch(vector<int>& nums,int l , int r,int target)
+    {
         while(l <= r)
         {
             int mid = l + (r-l)/2;
 
             if(nums[mid] == target) return mid;
-            // ascending
-            if(nums[mid] < nums[r])
-            {
-                if(target > nums[mid] && target <= nums[r])
-                    l = mid+1;
-                else
-                    r = mid-1;
-            }
+            if(nums[mid] > target) r = mid-1;
             else
-            {
-                if(target >= nums[l] && target < nums[mid])
-                    r = mid-1;
-                else 
-                    l = mid + 1;
-            }
+                l = mid + 1;
         }
         return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        
+        // find pivot element (pivot is smallest element)
+        // apply binary search to 2 parts (0,pivot-1) to (pivot to n-1)
+
+        int n = nums.size();
+        int pivot_indx = pivot(nums,n);
+        int idx = bsearch(nums,0,pivot_indx-1,target);
+        
+        if(idx != -1)
+            return idx;
+        
+        idx = bsearch(nums,pivot_indx,n-1,target);
+
+        return idx;
+
     }
 };
