d01cb8b860dfaec5752892811992a007bd6f7919
Adarsh Raj <132927594+AdarshRaj08@users.noreply.github.com>
2026-08-28 11:36:39 +0530

    Time: 932 ms (15.02%) | Memory: 178.9 MB (12.17%) - LeetSync

---
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    typedef vector<double> V;
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        int n = arr.size();
+
+        // min-heap {fraction, i, j}
+        priority_queue<V, vector<V>, greater<V>> pq;
+
+        for(int i=0; i<n; i++){
+            // denominator = arr[n-1]; // largest
+            pq.push({1.0*arr[i]/arr[n-1], (double)i, (double)n-1});
+        }
+
+        int smallest = 1; // first smallest
+        while(smallest < k){
+            
+            V vec = pq.top();
+            pq.pop();
+
+            int i = vec[1]; // index
+            int j = vec[2] - 1; // index
+
+            pq.push({1.0*arr[i]/arr[j], (double)i, (double)j});
+            smallest++;
+        }
+
+        V vec = pq.top();
+        int i = vec[1];
+        int j = vec[2];
+
+        return {arr[i], arr[j]};
+    }
+};
