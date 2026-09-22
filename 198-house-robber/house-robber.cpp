8973924fe74c5279d24ab895985c30abfc078797
Adarsh Raj <132927594+AdarshRaj08@users.noreply.github.com>
2026-09-22 19:09:02 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (6.19%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,53 +1,22 @@
 class Solution {
 public:
-    // dp[i] stores maximum money that can be robbed till index i
-    // WITHOUT robbing adjacent houses.
-    
-    // int amount(vector<int>&nums,int i, vector<int>&dp)
-    // {
-    //     if(i >= nums.size())
-    //         return 0;
+    int robb(vector<int>& nums, int idx, vector<int>&dp){
+        if(idx >= nums.size()){
+            return 0;
+        }
+        if(dp[idx] != -1){
+            return dp[idx];
+        }
         
-    //     if(dp[i] != -1)
-    //         return dp[i];
-    //     int take = nums[i] + amount(nums,i+2,dp);
-    //     int skip = amount(nums,i+1,dp);
+        // take
+        int take = nums[idx] + robb(nums, idx+2, dp); 
+        // skip
+        int skip = robb(nums, idx+1, dp);
+        return dp[idx] = max(take,skip);
+    }
 
-    //     return dp[i] = max(take,skip);
-    // }
-    // int rob(vector<int>& nums) {
-    //     int n = nums.size();
-
-    //     vector<int>dp(n,-1);
-
-    //     return amount(nums,0,dp);
-    // }
-
-
-    // **TABULATION METHOD** 
     int rob(vector<int>& nums) {
-        int n = nums.size();
-
-        if(n == 1) {
-            return nums[0];
-        }
-
-        vector<int> t(n+1,0);
-        // t[i] : denotes max stolen money till i house;
-
-        // no house
-        t[0] = 0;
-
-        t[1] = nums[0];
-
-        for(int i=2; i<=n; i++){
-            int steal = nums[i-1] + t[i-2];
-            int skip  = t[i-1];
-
-            t[i] = max(steal,skip);
-        }
-
-        return t[n];
-       
+        vector<int>dp(nums.size(),-1);
+        return robb(nums,0, dp);
     }
 };
